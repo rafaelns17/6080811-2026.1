9e902bf5320ea47c34a95681e618cc50ccb74e22
rafaelns17 <rafael.ns17@aluno.ifsc.edu.br>
2026-04-03 20:03:49 +0000

    para meu momo

--- start.js
+++ start.js
@@ -5,12 +5,13 @@ class start extends Phaser.Scene {
 
   preload() {
     this.load.setPath("assets/");
-    this.load.image("start", "start-background.png");
+    this.load.image("start", "start.png");
   }
 
   create() {
     this.add
       .image(400, 225, "start")
+      .setScale(0.5)
       .setInteractive()
       .on("pointerdown", () => {
         this.scene.start("scene0");
